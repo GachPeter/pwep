4cf76c632f1acae6d3e3ec988734fc62202edf94
gachpeter2@gmail.com <67166454+GachPeter@users.noreply.github.com>
2021-05-15 18:17:23 +0300

    from post.time to post._id

--- public/javascripts/mjs.js
+++ public/javascripts/mjs.js
@@ -38,7 +38,7 @@ function arrangeDocs(params) {
 
         }
         else if (post.show == 'on') {
-            document.getElementById("test").innerHTML = document.getElementById("test").innerHTML + "<div class='card my-5'><img src='https://main-pwep-ldhnayhlcqhu1y2u-gtw.qovery.io/posts/uploads/" + post.img + "' alt='Inspire' class='img-fluid'> <div class='card-body'><h5 class='card-title'>" + post.title + "</h5><div class='card-content'>  <p class='card-text'>" + post.content + "</p></div><hr><div class='row'><div class='col-4'><a class='btn btn-danger' href='http://localhost:3000/posts/delete/" + post.time + "' type='button'>Delete</a></div><div class='col-6'><div class='mb-1 text-muted' style='position:absolute;right:1.5em'>" + pd + "</div><div></div></div></div>"
+            document.getElementById("test").innerHTML = document.getElementById("test").innerHTML + "<div class='card my-5'><img src='https://main-pwep-ldhnayhlcqhu1y2u-gtw.qovery.io/posts/uploads/" + post.img + "' alt='Inspire' class='img-fluid'> <div class='card-body'><h5 class='card-title'>" + post.title + "</h5><div class='card-content'>  <p class='card-text'>" + post.content + "</p></div><hr><div class='row'><div class='col-4'><a class='btn btn-danger' href='https://main-pwep-ldhnayhlcqhu1y2u-gtw.qovery.io/posts/delete/" + post._id + "' type='button'>Delete</a></div><div class='col-6'><div class='mb-1 text-muted' style='position:absolute;right:1.5em'>" + pd + "</div><div></div></div></div>"
 
         }
     });
