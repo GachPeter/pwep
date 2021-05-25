83626785d5ca9b8b54f7fe39e817a92bac4bb2e8
gachpeter2@gmail.com <67166454+GachPeter@users.noreply.github.com>
2021-05-25 13:52:19 +0300

    c n

--- routes/manager.js
+++ routes/manager.js
@@ -4,7 +4,7 @@ var express = require('express');
 var router = express.Router();
 var session = require('express-session');
 var cors = require("cors");
-var MongoStore = require('mongo-store');
+var MongoStore = require('connect-mongo');
 
 var myerr = '';
 
